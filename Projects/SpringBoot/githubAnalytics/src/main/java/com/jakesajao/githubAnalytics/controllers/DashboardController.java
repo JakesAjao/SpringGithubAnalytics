b5816d4bbf5de3d5eb1dff578d4b0aa76cae2dac
Jacob.Ajao <jacajao09@gmail.com>
2021-09-17 19:55:12 +0100

    HTTPCOnnection Refactor

--- Projects/SpringBoot/githubAnalytics/src/main/java/com/jakesajao/githubAnalytics/controllers/DashboardController.java
+++ Projects/SpringBoot/githubAnalytics/src/main/java/com/jakesajao/githubAnalytics/controllers/DashboardController.java
@@ -2,29 +2,26 @@ package com.jakesajao.githubAnalytics.controllers;
 
 import com.jakesajao.githubAnalytics.models.GitUser;
 import com.jakesajao.githubAnalytics.models.Repository;
-import com.jakesajao.githubAnalytics.models.Root;
 import com.jakesajao.githubAnalytics.repositories.UserRepository;
+import com.jakesajao.githubAnalytics.services.HTTPConnections;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 
 @Controller
 public class DashboardController {
     @Autowired
     private UserRepository userRepository;
-    final String Git_URL = "https://api.github.com";
 
+    private HTTPConnections httpconnections = new HTTPConnections();
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
     @RequestMapping(value="/index",method= RequestMethod.POST)
     public ModelAndView index(@ModelAttribute("user") GitUser gituser) {
         System.out.println("Username from UI = "+gituser.getEmail());
@@ -32,44 +29,26 @@ public class DashboardController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
         String name = gitUser.getFirstName() +" "+ gitUser.getLastName();
-        List<Repository> repoList = getUserRepo(gitUser.getFirstName()+gitUser.getLastName());
+        List<Repository> repoList = httpconnections.getUserRepo(gitUser.getFirstName()+gitUser.getLastName());
         modelAndView.addObject("repolist",repoList);
         modelAndView.addObject("gituser",name);
 
         return modelAndView;
     }
 
-private List<Repository>  getUserRepo(String username) {
+    @RequestMapping(value="/repository",method= RequestMethod.POST)
+    public ModelAndView repository(@ModelAttribute("user") GitUser gituser,
+                                   @PathVariable("name2")String name2) {
+        System.out.println("Username from UI = "+gituser.getEmail());
+        GitUser gitUser = userRepository.findByEmail(gituser.getEmail());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        String name = gitUser.getFirstName() +" "+ gitUser.getLastName();
+        List<Repository> repository = httpconnections.repositoryByRepoName(gitUser.getFirstName()+gitUser.getLastName(),name2);
+        modelAndView.addObject("repository",repository);
+        modelAndView.addObject("gituser",name);
 
-    ArrayList<Repository> repoList = new ArrayList<>();
-
-    System.out.println("Repo model username: "+username);
-    String URL = Git_URL+"/users/" + username +"/repos?type=owner";
-
-    RestTemplate restTemplate = new RestTemplate();
-    HttpHeaders headers = new HttpHeaders();
-    headers.set("User-Agent", "profile-analyzer");
-    HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-
-    ResponseEntity<Root[]> repos = restTemplate.exchange(URL, HttpMethod.GET, entity, Root[].class);
-    //System.out.println("Git hub Data: "+repos);
-
-    Root[] arr = repos.getBody();
-    String description =null;
-    String name = null;
-    int id = 0;
-    for (Root repo : arr) {
-        id  =  ++id;
-        description = repo.getDescription();
-        name = repo.getName();
-        Repository repoData = new Repository(id,name,description);
-        //System.out.println("Git hub Data Repository: "+repoData);
-        repoList.add(repoData);
+        return modelAndView;
     }
-    //model.addAttribute("repolist",repoList);
-    return repoList;
-
-}
-
 
 }
---
+++ Projects/SpringBoot/githubAnalytics/src/main/java/com/jakesajao/githubAnalytics/services/HTTPConnections.java
@@ -0,0 +1,76 @@
+package com.jakesajao.githubAnalytics.services;
+
+import com.jakesajao.githubAnalytics.models.Repository;
+import com.jakesajao.githubAnalytics.models.Root;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HTTPConnections {
+    final String Git_URL = "https://api.github.com";
+    public List<Repository> getUserRepo(String username) {
+
+        ArrayList<Repository> repoList = new ArrayList<>();
+
+        System.out.println("Repo model username: "+username);
+        String URL = Git_URL+"/users/" + username +"/repos?type=owner";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("User-Agent", "profile-analyzer");
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+        ResponseEntity<Root[]> repos = restTemplate.exchange(URL, HttpMethod.GET, entity, Root[].class);
+        //System.out.println("Git hub Data: "+repos);
+
+        Root[] arr = repos.getBody();
+        String description =null;
+        String name = null;
+        int id = 0;
+        for (Root repo : arr) {
+            id  =  ++id;
+            description = repo.getDescription();
+            name = repo.getName();
+            Repository repoData = new Repository(id,name,description);
+            //System.out.println("Git hub Data Repository: "+repoData);
+            repoList.add(repoData);
+        }
+        //model.addAttribute("repolist",repoList);
+        return repoList;
+
+    }
+    public List<Repository> repositoryByRepoName(String username,String name){
+        System.out.println("Username from UI 2 = "+username);
+        ArrayList<Repository> repoList = new ArrayList<>();
+        System.out.println("Repo model username: "+username);
+        String URL = Git_URL+"/repos/" + username +"/"+name;
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("User-Agent", "profile-analyzer");
+        HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+        ResponseEntity<Root[]> repos = restTemplate.exchange(URL, HttpMethod.GET, entity, Root[].class);
+        //System.out.println("Git hub Data: "+repos);
+
+        Root[] arr = repos.getBody();
+        String description =null;
+        String name2 = null;
+        int id = 0;
+        for (Root repo : arr) {
+            id  =  ++id;
+            description = repo.getDescription();
+            name2 = repo.getName();
+            Repository repoData = new Repository(id,name2,description);
+            //System.out.println("Git hub Data Repository: "+repoData);
+            repoList.add(repoData);
+        }
+        return repoList;
+    }
+
+}
